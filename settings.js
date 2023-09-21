19a2334dd1bdec18cb1debc94839b8a6358e732a
CYBERXVVIP <145467751+CYBERXVVIP@users.noreply.github.com>
2023-09-21 15:34:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,14 +36,14 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['918590745203'] //ur owner number
+global.vcardowner = ['94741366868'] //ur owner number
 global.ownername = "ᴄyʙᴇʀᴛʀᴏɴ ɢᴏᴅᴢᴢᴢ" //ur owner name
 global.ytname = "YT:ᴄyʙᴇʀᴛʀᴏɴ ɢᴏᴅᴢᴢᴢ" //ur yt chanel name
 global.socialm = "GitHub: cybertrongodzzz" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['918590745203']
+global.owner = ['94741366868']
 global.ownertag = '918590745203' //ur tag number
 global.botnumber = '918590745203' //Add your Bot Number For Log
 global.botname = 'CYBER-X BETA EDITION' //ur bot name
